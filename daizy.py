13503af2138e60a61b67e615b6a0844747aa5426
Artalus <artalus-mail@yandex.ru>
2019-08-25 21:40:50 +0300

    working bot prototype

---
+++ daizy.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python3
+
+import vk_api as vk
+import vk_api.bot_longpoll as vkblp
+import vk_api.longpoll as vklp
+from vk_api.utils import get_random_id
+
+import os
+import json
+import bisect
+import re
+import queue
+import threading
+import twit
+import requests
+import time
+import traceback
+
+def chunker(seq, size):
+    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+
+
+def get_api():
+    me = json.load(open('me.json'))
+    apikey = me['apikey']
+    group = me['group']
+    owner = me['owner']
+
+    v = vk.VkApi(token=apikey, api_version='5.101')
+    api = v.get_api()
+    gid = api.utils.resolveScreenName(screen_name=group)['object_id']
+    bot = vkblp.VkBotLongPoll(v, group_id=gid)
+    return bot, v, api, gid, owner
+
+bot, _, api, gid, owner = get_api()
+print(bot)
+print(api)
+print(gid)
+print(owner)
+
+def json_or_default(jfile, default):
+    if not os.path.isfile(jfile):
+        return default
+    return json.load(open(jfile))
+
+SUBFILE='subscribers.json'
+def get_subscribers():
+    x = json_or_default(SUBFILE, [])
+    assert isinstance(x, list)
+    if x:
+        assert isinstance(x[0], int)
+    return x
+
+def ensure_subscribers_relevant(subs, chunksize=20):
+    for chunk in chunker(subs, chunksize):
+        cs = len(chunk)
+        x = api.messages.getConversations()
+        if x['count'] != chunksize:
+            if cs == 1:
+                del subs[0]
+            else:
+                c2 = cs//2
+                ensure_subscribers_relevant(subs[:c2], c2)
+                ensure_subscribers_relevant(subs[c2:], c2)
+
+subscribers = get_subscribers()
+
+
+def add_subscriber(peer_id: int):
+    if peer_id in subscribers:
+        print(f'{peer_id} already subscribed; ignoring')
+        return
+    bisect.insort(subscribers, peer_id)
+    with open(SUBFILE, 'w') as f:
+        json.dump(subscribers, f)
+def remove_subscriber(peer_id: int):
+    if peer_id not in subscribers:
+        print(f'{peer_id} not subscribed; ignoring')
+        return
+    subscribers.remove(peer_id)
+    with open(SUBFILE, 'w') as f:
+        json.dump(subscribers, f)
+
+
+def send_to(peer_id, message):
+    try:
+        api.messages.send(
+            peer_id=peer_id,
+            random_id=get_random_id(),
+            message=message
+        )
+    except vk.ApiError as e:
+        print(f'Failed to send "{message[:100]}"... to {peer_id}:\n{e.error}')
+        if e.code == 7:
+            print('Permission denied, seems like bot was removed from this conversation')
+            print(f'Removing {peer_id} from subscribers')
+            remove_subscriber(peer_id)
+
+COMMAND_RE = re.compile(fr'\[club{gid}\|.+?\] *\/(.+)')
+def try_extract_command(msg):
+    x = COMMAND_RE.search(msg)
+    if x:
+        return x.group(1)
+
+def is_invitation(event):
+    if 'action' in event.object and event.object.action['type'] == 'chat_invite_user':
+        return True
+    cmd = try_extract_command(event.object.text)
+    if cmd and cmd == 'start':
+        return True
+    return False
+
+
+def listen_for_messages():
+    for e in bot.check():
+        if e.type == vkblp.VkBotEventType.MESSAGE_NEW:
+            yield e
+
+
+def get_last_twits() -> dict:
+    x = json_or_default('last_twits.json', {})
+    assert(isinstance(x, dict))
+    for a,b in x.items():
+        assert(isinstance(a, str))
+        assert(isinstance(b, int))
+    return x
+
+last_twits = get_last_twits()
+
+def last_twit(author: str):
+    if author in last_twits:
+        return last_twits[author]
+    return 0
+
+def update_last_twit(author: str, twid: int):
+    write = False
+    if author not in last_twits:
+        print(f'No known twits from {author}, adding {twid}')
+        write = True
+    elif twid > last_twits[author]:
+        print(f'{twid} is newer for {author} than {last_twits[author]}, updating')
+        write = True
+    if write:
+        last_twits[author] = twid
+        json.dump(last_twits, open('last_twits.json', 'w'))
+
+
+def new_twits(author: str):
+    twits = []
+    for t in twit.tweets_from_web(author):
+        if t.twid > last_twit(author):
+            twits.append(t)
+        else:
+            break
+    if twits:
+        update_last_twit(author, twits[0].twid)
+    return twits
+
+
+def main():
+    for chunk in chunker(subscribers, 20):
+        ss = ', '.join(map(str, chunk))
+        send_to(owner, f'bot online in {ss}')
+
+    while True:
+        try:
+            print('vk iteration...')
+            for event in listen_for_messages():
+                print(event)
+                if is_invitation(event):
+                    pi = event.object.peer_id
+                    add_subscriber(pi)
+                    send_to(pi, f'Added to {pi}')
+            print('twitter iteration...')
+            for twitch in chunker(new_twits('cnn'), 7):
+                msg = f'\n\n{"-"*10}\n\n'.join(map(str, twitch))
+                for sub in subscribers[:]:
+                    send_to(sub, msg)
+        except Exception as e:
+            print('SOMETHING HAPPENED:')
+            traceback.print_exc()
+            print("sleeping...")
+            time.sleep(30)
+
+
+    print("finishing")
+
+if __name__ == "__main__":
+    main()
---
+++ twit.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+from bs4 import BeautifulSoup
+import bs4
+import requests
+
+
+def tag_to_str(x):
+    if isinstance(x, bs4.element.NavigableString):
+        return str(x).strip()
+    for a in ('data-expanded-url', 'data-url', 'href'):
+        if x.has_attr(a):
+            link = x[a]
+            break
+    lf = '\n'
+    txt = ''
+    if link[0] == '/':
+        link = f'https://twitter.com{link}'
+        lf = ' '
+        txt = f'| {x.text.strip()}'
+    if x.name == 'a':
+        return '{lf}{}{}{lf}'.format(link, txt, lf=lf)
+    return str(x)
+
+class Twit:
+    def __init__(self, twid: str, text: str, author: str):
+        self.twid = int(twid)
+        self.text = text.strip()
+        self.author = author
+
+    @staticmethod
+    def from_soup(tt, author):
+        twid = tt["data-id"]
+        txt = tt.select('div[class*="dir-ltr"]')[0]
+        message = ''
+        for x in txt:
+            message += tag_to_str(x)
+        return Twit(twid, message, author)
+
+    def __str__(self):
+        return (
+            f'{self.author} @ https://twitter.com/{self.author}/status/{self.twid}\n\n'
+            f'{self.text}')
+
+def tweets_from_string(html_doc, author):
+    soup = BeautifulSoup(html_doc, 'lxml')
+    return (Twit.from_soup(tt, author) for tt in soup.select('div[class*="tweet-text"]'))
+
+def tweets_from_web(author):
+    content = requests.get(f'https://mobile.twitter.com/{author}').content
+    open('twitter_last.html', 'w').write(content.decode('utf-8'))
+    twits = tweets_from_string(content, author)
+    if not twits:
+        print('!! SOMETHING BAD HAPPENED WITH TWITTER !!')
+        print('SEE twitter_last.html FILE')
+    return twits
+
+def main():
+    tweets = tweets_from_string(open("twit/dayzf2.xml").read())
+    tweets = tweets_from_web()
+    for twit in tweets:
+        print(twit)
+        print('-'*20)
+
+if __name__ == "__main__":
+    main()
